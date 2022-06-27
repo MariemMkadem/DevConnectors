9937c0dd239985c58582f7e1b3da8788615b2d82
mariem mkadem <mmkadem11@gmail.com>
2022-06-27 23:07:58 +0100

    authentication

--- DEVCONNECTOR/routes/api/auth.js
+++ DEVCONNECTOR/routes/api/auth.js
@@ -1,4 +1,8 @@
 const express = require('express')
+const { check, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+const config = require('config')
+const jwt = require('jsonwebtoken')
 const auth = require('../../middleware/auth')
 const User = require('../../models/User')
 
@@ -18,5 +22,59 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server Error')
   }
 })
+
+// @routes    Post api/auth
+// @desc      authenticate user
+// @access    Public
+router.post(
+  '/',
+  [
+    check('email', 'Please set e valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() })
+    }
+    const { email, password } = req.body
+    try {
+      let user = await User.findOne({ email })
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] })
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password)
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] })
+      }
+
+      //return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      }
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err
+          res.json({ token })
+        }
+      )
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send('server error')
+    }
+  }
+)
 
 module.exports = router
