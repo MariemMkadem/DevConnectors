88bacef9dad33cb43376f64ed1282540e1a85804
mariem mkadem <mmkadem11@gmail.com>
2022-06-27 17:16:05 +0100

    add custom auth middleware

--- DEVCONNECTOR/routes/api/auth.js
+++ DEVCONNECTOR/routes/api/auth.js
@@ -1,11 +1,22 @@
-const express = require('express');
+const express = require('express')
+const auth = require('../../middleware/auth')
+const User = require('../../models/User')
 
-const router = express.Router();
+const router = express.Router()
 
 // @routes    Get api/auth
 // @desc      Test routes
 // @access    Public
 
-router.get('/', (req,res)=> res.send('auth routes'))
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-passord')
+    res.json(user)
+    res.send('auth routes')
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
 
-module.exports = router;
+module.exports = router
